38cd9e637de9946ab294aba29e64d8d63af5f8e7
JogenderGahlawat <Iamgahlawat777@gmail.com>
2026-06-04 08:58:58 +0530

    added data

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const MySQL = require('mysql2');
 
 const app = express();
-const PORT = 3000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -176,6 +176,5 @@ app.get('/api/search-hotels', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`\n🚀 Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
