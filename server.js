f482cd86d85b0f9f915c843d4bbc77f063b234e4
JogenderGahlawat <Iamgahlawat777@gmail.com>
2026-06-07 12:13:45 +0530

    a

--- server.js
+++ server.js
@@ -190,3 +190,7 @@ app.get('/api/search-hotels', async (req, res) => {
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_PORT =", process.env.DB_PORT);
